23c6e28041918396994f0238b85361ec8303348e
heStudio <msqlxx163.com@hotmail.com>
2022-03-12 04:41:12 +0800

    20220312

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+
+##换源
+
+sed -i 's@^\(deb.*stable main\)$@#\1\ndeb https://mirrors.tuna.tsinghua.edu.cn/termux/termux-packages-24 stable main@' $PREFIX/etc/apt/sources.list
+sed -i 's@^\(deb.*games stable\)$@#\1\ndeb https://mirrors.tuna.tsinghua.edu.cn/termux/game-packages-24 games stable@' $PREFIX/etc/apt/sources.list.d/game.list
+sed -i 's@^\(deb.*science stable\)$@#\1\ndeb https://mirrors.tuna.tsinghua.edu.cn/termux/science-packages-24 science stable@' $PREFIX/etc/apt/sources.list.d/science.list
+apt update -y && apt upgrade -y
+
+##安装
+
+pkg install git -y
+pkg install python -y
+git clone https://gitee.com/heStudio/ka_install.git
+cd ka_install
+chmod +x kalinethunter finaltouchup.sh
+./kalinethunter
+startkali
---
+++ readme.md
@@ -0,0 +1,13 @@
+#### Kali Linux Install for Android without Root
+
+#使用方法
+
+termux-setup-storage && curl -O --url http://www.hestudio.xyz/kali_for_android/install.sh && chmod +x install.sh && ./install.sh
+
+复制以上内容并粘贴在termux中。
+
+ 
+#官方文档
+https://www.kali.org/docs/nethunter/nethunter-rootless/
+
+
